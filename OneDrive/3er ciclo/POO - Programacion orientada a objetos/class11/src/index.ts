66feb80b2eea3f81cf6e1597c4b7e609de5da789
milethmdr <milethmdr06@gmailcom>
2026-02-24 10:18:56 -0600

    agregue archivos

---
+++ OneDrive/3er ciclo/POO - Programacion orientada a objetos/class11/src/index.ts
@@ -0,0 +1,144 @@
+ import * as readline from 'readline';
+
+//  class Programa {
+//      private rl: readline.Interface;
+
+//      constructor() {
+//          this.rl = readline.createInterface({
+//              input: process.stdin,
+//              output: process.stdout
+//          });
+//      }
+//    }
+
+//     iniciar(): void {
+//         this.pedirNumero();
+//     }
+
+//     pedirNumero(): void {
+//         this.rl.question("Ingresa un número: ", (respuesta: string) => {
+//             const numero = Number(respuesta);
+//             this.evaluarNumero(numero);
+//             this.cerrar();
+//         });
+//     }
+
+//     evaluarNumero(numero: number): void {
+//         if (numero > 10) {
+//             console.log("Es mayor que 10");
+//         } else if (numero === 10) {
+//             console.log("Es igual a 10");
+//         } else {
+//             console.log("Es menor que 10");
+//         }
+//       }
+//       cerrar():void{
+//          this.rl.close();
+//       }
+
+// }
+// const app = new Programa();
+// app.iniciar();
+
+// EJERCICIOS
+//  Desarrollar una aplicación en consola que permita ingresar la nota de un 
+// estudiante y determinar su estado académico. 
+// Usar if / else if para clasificar: 
+// • 9 – 10 = Excelente 
+// • 7 – 8.9 = Bueno 
+// • 6 – 6.9 = Regular 
+// • Menor a 6 = Reprobado
+
+// class ProgramaNotas {
+//     private rl: readline.Interface;
+
+//     constructor() {
+//         this.rl = readline.createInterface({
+//             input: process.stdin,
+//             output: process.stdout
+//         });
+//     }
+
+//     iniciar(): void {
+//         this.pedirNota();
+//     }
+
+//     pedirNota(): void {
+//         this.rl.question("Ingrese la nota del estudiante: ", (respuesta: string) => {
+//             const nota = Number(respuesta);
+//             this.evaluarNota(nota);
+//             this.cerrar();
+//         });
+//     }
+
+//     evaluarNota(nota: number): void {
+//         if (nota >= 9 && nota <= 10) {
+//             console.log("Excelente");
+//         } else if (nota >= 7 && nota < 9) {
+//             console.log("Bueno");
+//         } else if (nota >= 6 && nota < 7) {
+//             console.log("Regular");
+//         } else if (nota < 6) {
+//             console.log("Reprobado");
+//         } 
+//     }
+
+//     cerrar(): void {
+//         this.rl.close();
+//     }
+// }
+
+// const app = new ProgramaNotas();
+// app.iniciar();
+
+//2
+// Desarrollar un sistema de autenticación que valide usuario, contraseña.  
+// Si el login es correcto, mostrar el rol al que pertenece el usuario: 
+// • 1 = Administrador 
+// • 2 = Cliente 
+// • 3 =1 Invitado 
+// Si el usuario no existe, mostrar mensaje de usuario incorrecto.
+
+
+class Sistema {
+    private rl: readline.Interface;
+
+    constructor() {
+        this.rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+    }
+
+    iniciar(): void {
+        this.pedirUsuario();
+    }
+
+    pedirUsuario(): void {
+        this.rl.question("Usuario: ", (usuario: string) => {
+            this.rl.question("Contraseña: ", (clave: string) => {
+                this.validarLogin(usuario, clave);
+                this.cerrar();
+            });
+        });
+    }
+
+    validarLogin(usuario: string, clave: string): void {
+        if (usuario === "admin" && clave === "1234") {
+            console.log("Login correcto. Rol: Administrador");
+        } else if (usuario === "cliente" && clave === "abcd") {
+            console.log("Login correcto. Rol: Cliente");
+        } else if (usuario === "invitado" && clave === "aaee") {
+            console.log("Login correcto. Rol: Invitado");
+        } else {
+            console.log("Usuario incorrecto o contraseña inválida");
+        }
+    }
+
+    cerrar(): void {
+        this.rl.close();
+    }
+}
+
+const app = new Sistema();
+app.iniciar();
